21e3396f8512f1fdf0bd8a60cf86e8d076319ca5
AlbertoLaptop <albertramos902@gmail.com>
2023-05-23 18:11:32 +0200

    styles added

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
+const homeButton = document.getElementById('home');
+const aboutButton = document.getElementById('about');
+const githubButton = document.getElementById('github');
+const contactButton = document.getElementById('contact')
 const previousButton = document.getElementById('previous');
 const nextButton = document.getElementById('next');
+const footerLink = document.getElementById('footerLink');
 
 
 previousButton.addEventListener('click',alertButtonPrevious);
+
 nextButton.addEventListener('click', alertButtonNext);
+
+homeButton.addEventListener('click',navButtons);
+
+aboutButton.addEventListener('click', navButtons);
+
+githubButton.addEventListener('click', function(){
+    window.open(href="https://github.com/BertoRamosM", "_blank");
+})
+
+
+contactButton.addEventListener('click', navButtons);
+
+
+footerLink.addEventListener('click', function(){
+    window.open(href="https://github.com/BertoRamosM", "_blank");
+})
+
+
+
+function navButtons(){
+    alert("Still not available")
+}
 
 function alertButtonPrevious(){
     alert("Previous Page under maintenance");
@@ -12,3 +40,4 @@ function alertButtonPrevious(){
 function alertButtonNext(){
     alert("Next page under maintenance")
 }
+
